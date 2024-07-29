1763391ea59a119cb16ff7358b10c76c826aa867
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-07-29 15:29:32 +0200

    chore(aud): remove unused audience secret claims
    Signed-off-by: leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>

--- modules/iam_identity_provider/main.tf
+++ modules/iam_identity_provider/main.tf
@@ -3,18 +3,6 @@ data "tls_certificate" "tfc_certificate" {
   url = "https://${var.tfc_hostname}"
 }
 
-// generate two random strings to use as audience
-resource "random_string" "sfx1" {
-  length  = 5
-  special = false
-  upper   = false
-}
-resource "random_string" "sfx2" {
-  length  = 3
-  special = false
-  upper   = false
-}
-
 // create AWS IAM OIDC Identity Provider
 resource "aws_iam_openid_connect_provider" "this" {
   url             = data.tls_certificate.tfc_certificate.url
